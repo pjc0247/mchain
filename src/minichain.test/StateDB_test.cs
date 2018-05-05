d76ffc0e7997d480231025d3829aebcf75611240
Park June Chul <pjc0247@naver.com>
2018-05-05 22:50:58 +0900

    add tests

--- src/minichain.test/StateDB_test.cs
+++ src/minichain.test/StateDB_test.cs
@@ -9,6 +9,47 @@ namespace minichain.test
     public class StateDB_test
     {
         [TestMethod]
+        public void PushState()
+        {
+            var sdb = new StateDB(new MemDB());
+
+            var addr_a = UniqID.Generate();
+            var addr_b = UniqID.Generate();
+
+            var state = sdb.PushState(null, "1", new PushStateEntry[]
+            {
+                PushStateEntry.Create(PushStateFlag.None, 
+                     new SingleState(StateType.Wallet)
+                     {
+                         key = addr_a,
+                         balance = 100
+                     }),
+                PushStateEntry.Create(PushStateFlag.None,
+                     new SingleState(StateType.Wallet)
+                     {
+                         key = addr_b,
+                         balance = 200
+                     }),
+            });
+
+            Assert.AreEqual(100, sdb.GetState("1", addr_a).balance);
+            Assert.AreEqual(200, sdb.GetState("1", addr_b).balance);
+
+            state = sdb.PushState(state, "2", new PushStateEntry[]
+            {
+                PushStateEntry.Create(PushStateFlag.None,
+                     new SingleState(StateType.Wallet)
+                     {
+                         key = addr_a,
+                         balance = 200
+                     })
+            });
+
+            Assert.AreEqual(200, sdb.GetState("1", addr_a).balance);
+            Assert.AreEqual(200, sdb.GetState("1", addr_b).balance);
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
         public void NoDuplicatedEntry()
         {
