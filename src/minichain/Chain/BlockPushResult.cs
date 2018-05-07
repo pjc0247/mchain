82e2696a86845cc5579e01178be13f967d4607de
Park June Chul <pjc0247@naver.com>
2018-05-07 19:07:14 +0900

    a

---
+++ src/minichain/Chain/BlockPushResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minichain
+{
+    internal enum BlockPushResultCode
+    {
+        OK,
+
+        FromAnotherBranch,
+        InvalidBlock
+    }
+
+    internal struct BlockPushResult
+    {
+        public BlockPushResultCode code;
+
+        public string branchedBlockHash;
+
+        public static BlockPushResult OK()
+        {
+            return new BlockPushResult(BlockPushResultCode.OK);
+        }
+
+        public BlockPushResult(BlockPushResultCode _code)
+        {
+            code = _code;
+            branchedBlockHash = null;
+        }
+        public BlockPushResult(BlockPushResultCode _code, string _branchedBlockHash)
+        {
+            code = _code;
+            branchedBlockHash = _branchedBlockHash;
+        }
+    }
+}
