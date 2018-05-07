277d5d74b71265fa5340e319ff466a7e6b58c589
Park June Chul <pjc0247@naver.com>
2018-05-07 20:15:34 +0900

    KeyValueDB

---
+++ src/minichain/DB/KeyValueDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minichain
+{
+    public class KeyValueDB
+    {
+        private string prefix = "";
+        private IStorageBackend db;
+
+        public KeyValueDB()
+        {
+            db = new FileDB();
+        }
+        public KeyValueDB(string _prefix, IStorageBackend _db)
+        {
+            prefix = _prefix;
+            db = _db;
+        }
+
+        public void Set(string key, object value)
+        {
+            db.Write(prefix + key, value);
+        }
+        public T Get<T>(string key)
+        {
+            return db.Read<T>(prefix + key);
+        }
+        public string Get(string key)
+        {
+            return db.Read<string>(prefix + key);
+        }
+    }
+}
