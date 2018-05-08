50de6d6ea8d8d323263c31a8a0e8c750f3da406c
Park June Chul <pjc0247@naver.com>
2018-05-09 00:51:21 +0900

    add tests

---
+++ src/minichain.test/dbtest/KeyValueDB_test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using minichain;
+
+namespace minichain.test
+{
+    [TestClass]
+    public class KeyValueDB_test
+    {
+        [TestMethod]
+        public void SaveAndLoad()
+        {
+            var db = new KeyValueDB();
+
+            db.Set("a", "HELLO");
+            db.Set("b", "BYE");
+
+            Assert.AreEqual("HELLO", db.Get("a"));
+            Assert.AreEqual("BYE", db.Get("b"));
+        }
+    }
+}
---
+++ src/minichain.test/dbtest/MemDB_test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using minichain;
+
+namespace minichain.test
+{
+    [TestClass]
+    public class MemDB_test
+    {
+        [TestMethod]
+        public void SaveAndLoad()
+        {
+            var db = new MemDB();
+
+            db.Write("a", "HELLO");
+            db.Write("b", "BYE");
+
+            Assert.AreEqual("HELLO", db.Read<string>("a"));
+            Assert.AreEqual("BYE", db.Read<string>("b"));
+        }
+
+        [TestMethod]
+        public void StashAndRead()
+        {
+            var db = new MemDB();
+
+            db.Write("a", "HELLO");
+            db.Stash("a");
+
+            Assert.AreEqual(null, db.Read<string>("a"));
+        }
+    }
+}
